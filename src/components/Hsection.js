1143e499448be2218ac0c124d43b4b136cd83dcb
Anouxay Sichanh <ainthaly@gmail.com>
2023-02-14 20:00:02 -0500

    firebase auth & database added

--- src/components/Hsection.js
+++ src/components/Hsection.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import '../App.scss';
+import { Button } from './Button';
+import './Hsection.scss';
 
-function HSection() {
+
+function Hsection() {
     return (
         <div className='h-container'>
-            <video src="/videos/video-2.mp4"></video>
+            {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
+
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for?</p>
+            <div className='h-btns'>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                >
+                    GET STARTED
+                </Button>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--primary'
+                    buttonSize='btn--large'
+                    onClick={console.log('hey')}
+                >
+                    WATCH TRAILER <i className='far fa-play-circle' />
+                </Button>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Hsection;
