c4370f2d591d5d60edd21bee4f6c423d4cd5ae0c
Anouxay Sichanh <ainthaly@gmail.com>
2023-02-18 16:15:20 -0500

    testing different codes out

---
+++ src/components/__tests__/util.test.js
@@ -0,0 +1,15 @@
+const { generateText, checkAndGenerate } = require('./util2');
+test('should output name and age', () => {
+    const text = generateText('James', 34);
+    expect(text).toBe('James (34 years old)');
+});
+
+test('should output data-less text', () => {
+    const text = generateText('', null);
+    expect(text).toBe(' (null years old)')
+})
+
+test('should generate a valid text output', () => {
+    const text = checkAndGenerate('James', 34);
+    expect(text).toBe('James (34 years old)')
+});
