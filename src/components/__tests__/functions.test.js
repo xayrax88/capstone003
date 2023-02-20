fcce72404224d75c320d8bcf93f5539f40358e26
Anouxay Sichanh <ainthaly@gmail.com>
2023-02-20 14:25:24 -0500

    removed webpack.config

--- src/components/__tests__/functions.test.js
+++ src/components/__tests__/functions.test.js
@@ -31,13 +31,6 @@ describe('Checking Names', () => {
 
 
 //-----------------
-test('Adds 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
-});
-test('Adds 2 + 2 to NOT equal 5', () => {
-    expect(functions.add(2, 2)).not.toBe(5);
-});
-
 //working with async data
 //Promise 
 // test('User fetched name should be Leanne Graham', () => {
