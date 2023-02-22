6a8ecf5f1a7d6d43e61a270a1263b0156e20a3cf
Anouxay Sichanh <ainthaly@gmail.com>
2023-02-22 11:56:52 -0500

    Fixed Signup & Login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -2,44 +2,71 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth, logInWithEmailAndPassword } from "../../Firebase/Firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import "./Login.scss";
+import {
+  MDBInput,
+  MDBRow,
+  MDBBtn,
+  MDBCol,
+  MDBContainer,
+} from 'mdb-react-ui-kit';
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
 
-  useEffect(()=>{
+  useEffect(() => {
     if (user) navigate("/dashboard");
-  },[user])
+  }, [user])
 
- return (
-    <div className="login">
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        logInWithEmailAndPassword(email, password)
-      }}>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          className="login__btn"
-          type="submit"
-        >
-          Login
-        </button>
-        <div>
-          <span>Don't have an account? <Link to="/sign-up">Register</Link> now</span>
-        </div>
-      </form>
-    </div>
+  return (
+    <MDBContainer className='mt-3 d-flex justify-content-center'>
+      <MDBRow>
+        <MDBCol>
+          <form onSubmit={(e) => {
+            e.preventDefault()
+            logInWithEmailAndPassword(email, password)
+          }}>
+
+            <MDBInput
+              className='mb-4'
+              type='email'
+              id='form9Example1'
+              label='Email'
+              placeholder='Email'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+
+
+            <MDBInput
+              wrapperClass='mb-4'
+              type='password'
+              id='form9Example2'
+              label='Password'
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+
+
+
+            <MDBBtn type='submit' className='mb-4' block>
+              Login
+            </MDBBtn>
+
+
+            <div className='text-center'>
+              <p>
+                Not a member? <Link to='/sign-up'>Register</Link>
+              </p>
+            </div>
+
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
 export default Login;
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link, useNavigate} from "react-router-dom";
-import styles from '../SignUp.scss'
-
+import { Link, useNavigate } from "react-router-dom";
 import {
   auth,
   registerWithEmailAndPassword,
 } from "../../Firebase/Firebase";
-function Register() {
+
+// import styles from '../SignUp.scss'
+import {
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBContainer,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
+
+export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   const [name, setName] = useState("");
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
@@ -20,48 +29,58 @@ function Register() {
 
   useEffect(() => {
     if (loading) return;
-    if(user)
-    {
+    if (user) {
       return navigate("/dashboard");
     }
   }, [user, loading]);
 
   return (
-    <div className="register">
-      <div className="register__container">
-        <input
-          type="text"
-          className={styles.register__textBox}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Full Name"
-        />
-        <br/>
-        <input
-          type="text"
-          className="register__textBox"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="E-mail Address"
-        />
-        <br/>
-        <input
-          type="password"
-          className="register__textBox"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-         <br/>
-        <button className="register__btn" onClick={register}>
-          Register
-        </button>
-         <br/>
-        <div>
-          Already have an account? <Link to="/login">Login</Link> now.
-        </div>
-      </div>
-    </div>
+
+    <MDBContainer className='mt-3 d-flex justify-content-center'>
+      <MDBRow>
+        <MDBCol>
+          <form>
+            <MDBInput
+              id='form3Example1'
+              label='Name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name"
+            />
+
+            <MDBInput
+              className='mt-2 mb-4'
+              type='email'
+              id='form3Example3'
+              label='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+            />
+
+            <MDBInput
+              className='mb-4'
+              type='password'
+              id='form3Example4'
+              label='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+
+
+            <MDBBtn
+              type='submit'
+              className='mb-4'
+              block
+              onClick={register}
+            >
+              Sign Up
+            </MDBBtn>
+
+          </form>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 }
-export default Register;
